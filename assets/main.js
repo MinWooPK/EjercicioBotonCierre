6bd504493b6489a7f884077817e8ea17031039e4
MinWooPK <minwoowork91@gmail.com>
2022-11-03 16:18:21 +0100

    main

---
+++ assets/main.js
@@ -0,0 +1,23 @@
+// s1img.onclick = function () {
+//     s1.hidden = true
+// }
+
+// s2img.onclick = function () {
+//     s2.hidden = true
+// }
+
+// s3img.onclick = function () {
+//     s3.hidden = true
+// }
+
+let windowse = document.querySelectorAll("Div");
+for (let i = 0; i < windowse.length; i++) {
+    
+
+    
+    console.log(windowse)
+}
+
+// let DIVI = document.body.querySelectorAll("Div")
+// console.log(DIVI)
+// };
